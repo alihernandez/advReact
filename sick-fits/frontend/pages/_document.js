52c7ca66f08ff006a88fb6c25201374c9fe38be6
phantomramen <ali.hernandez9two@gmail.com>
2022-01-08 15:19:36 -0700

    fixed rendering issue

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,7 +1,14 @@
 // global default document template
 import Document, { Html, Head, NextScript, Main} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
     // classes in react have a render method rather than a return
     render() {
         return (
