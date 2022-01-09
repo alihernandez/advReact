1c39e9541ca56142d87b4ef8885ed4e12f43a5a0
phantomramen <ali.hernandez9two@gmail.com>
2022-01-08 19:27:26 -0700

    created first user

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,8 @@
+import { createAuth } from '@keystone-next/auth';
 import { User } from './schemas/User';
 import 'dotenv/config';
 import { config, createSchema} from '@keystone-next/keystone/schema';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -10,8 +12,18 @@ const sessionConfig = {
     secret: process.env.COOKIE_SECRET,
 };
 
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        // TODO Add in initial roles
+    }
+});
 
-export default config({
+
+export default withAuth(config({
     server: {
         cors: {
             origin: [process.env.FRONTEND_URL],
@@ -25,11 +37,19 @@ export default config({
     },
     lists: createSchema({
         //Schema items go here
-        User
+        User,
     }),
     ui: {
-        //change for roles
-        isAccessAllowed: () => true,
+        //Show UI only for people who pass this test
+        isAccessAllowed: ({ session }) => {
+            // console.log(session);
+            return !!session?.data;
+        }
     },
-    // TODO add session values here
-    });
+    // passes along ID with every session
+    session: withItemData(statelessSessions(sessionConfig), {
+        //GraphQL query
+        User: `id name email`
+    })
+    })
+    );
