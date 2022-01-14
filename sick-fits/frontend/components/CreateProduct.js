42ee137836cef87d020d1dd8e4d3afdb690bbd96
phantomramen <ali.hernandez9two@gmail.com>
2022-01-14 15:46:13 -0700

    implemented adding products via mutations

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,19 +1,59 @@
+import { useMutation } from '@apollo/client';
+import gql from "graphql-tag";
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+mutation CREATE_PRODUCT_MUTATION(
+    # which variables are being passed in? and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+){
+  createProduct(
+      data: {
+    name: $name
+    description: $description
+    price: $price
+    status: "AVAILABLE"
+    photo: { create: { image: $image, altText: $name } }
+  }
+  ) {
+    id
+    price
+    description
+    name
+  }
+}
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
     image: "",
     name: "Nice Shoes",
-    price: 12345,
+    price: 34500,
     description: "AGGGGG",
   });
+  const [createProduct, { loading, error, data}] = useMutation(
+      CREATE_PRODUCT_MUTATION, 
+      {
+      variables: inputs,
+  }
+  );
   return (
-    <Form onSubmit={(e) => {
+    <Form onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
-    }}>
-      <fieldset>
+        //Submit input fields to backend:
+        await createProduct();
+        // console.log(res);
+        clearForm();
+    }}
+    >
+        <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
       <label htmlFor="image">
           Image
           <input
